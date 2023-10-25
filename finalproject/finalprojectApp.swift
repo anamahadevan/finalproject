54963f55f906bf3c96faf634ffd8dc183d02b36a
ana <anabellemahadevan@gmail.com>
2023-10-24 21:44:06 -0600

    Initial Commit

---
+++ finalproject/finalprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalprojectApp.swift
+//  finalproject
+//
+//  Created by anabelle mahadevan on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct finalprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
