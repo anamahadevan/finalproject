32be86e7be6568831b29778f9a67dcd4fe08df16
ana <anabellemahadevan@gmail.com>
2023-12-18 10:25:53 -0600

    corre ction

--- finalproject/Views/SettingsView.swift
+++ finalproject/Views/SettingsView.swift
@@ -18,7 +18,7 @@ struct SettingsView: View {
                     .listRowBackground(Color.background)
                 
                 Section{
-                    Text("The Pomodoro Technique is a time management method based on 25-minute stretches of focused work broken by five-minute breaks. Longer breaks, typically 15 to 30 minutes, are taken after four consecutive work intervals. Each work interval is called a pomodoro, the Italian word for tomato ")
+                    Text("The Pomodoro Technique is a time management method based on 25-minute stretches of focused work broken by five-minute breaks. Then longer breaks, typically 15 to 30 minutes. Each work interval is called a pomodoro, the Italian word for tomato ")
                 }.listRowBackground(Color.accent)
                 
                 Section(header: Image("timersettings").foregroundColor(.white)){
